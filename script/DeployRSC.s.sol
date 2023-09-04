18cb20146d7ca32ba3e7b43b3da1526e08ba4718
DeograciousAggrey <deograciousaggrey@gmail.com>
2023-09-04 21:04:10 +0300

    Updated the Deployment script

--- script/DeployRSC.s.sol
+++ script/DeployRSC.s.sol
@@ -5,12 +5,29 @@ pragma solidity ^0.8.19;
 import {Script, console} from "forge-std/Script.sol";
 import {RomanStableCoin} from "../src/RomanStableCoin.sol";
 import {RSCEngine} from "../src/RSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployRSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function run() external returns (RomanStableCoin, RSCEngine) {
-        vm.startBroadcast();
+        HelperConfig helperConfig = new HelperConfig();
+
+        (address wethUsdPriceFeed, address wbtcUsdPriceFeed, address weth, address wbtc, uint256 deployerKey) =
+            helperConfig.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
         RomanStableCoin rsc = new RomanStableCoin();
+        RSCEngine rscEngine = new RSCEngine(tokenAddresses, priceFeedAddresses, address(rsc));
+
+        rsc.transferOwnership(address(rscEngine));
 
         vm.stopBroadcast();
+
+        return (rsc, rscEngine);
     }
 }
