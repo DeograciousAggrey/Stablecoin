6beace31af79afcb0a9e7b60e9ba9a249a8b8514
DeograciousAggrey <deograciousaggrey@gmail.com>
2023-09-05 16:18:29 +0300

    Updated the tests

--- test/unit/RSCEngineTest.t.sol
+++ test/unit/RSCEngineTest.t.sol
@@ -7,6 +7,7 @@ import {RSCEngine} from "../../src/RSCEngine.sol";
 import {RomanStableCoin} from "../../src/RomanStableCoin.sol";
 import {DeployRSC} from "../../script/DeployRSC.s.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
 
 contract RSCEngineTest is Test {
     DeployRSC deployer;
@@ -16,10 +17,16 @@ contract RSCEngineTest is Test {
     address ethUsdPriceFeed;
     address weth;
 
+    address public USER = makeAddr("user");
+    uint256 public constant AMOUNT_COLLATERAL = 10000 ether;
+    uint256 public constant STARTING_USER_BALANCE = 100 ether;
+
     function setUp() public {
         deployer = new DeployRSC();
         (rsc, rscEngine, helperConfig) = deployer.run();
         (ethUsdPriceFeed,, weth,,) = helperConfig.activeNetworkConfig();
+
+        ERC20Mock(weth).mint(USER, STARTING_USER_BALANCE);
     }
 
     ////////////////////////////////////////////////////
@@ -32,5 +39,18 @@ contract RSCEngineTest is Test {
         uint256 expectedUsdValue = 30000e18;
         uint256 actualUsdValue = rscEngine.getUsdValue(weth, ethAmount);
         assertEq(actualUsdValue, expectedUsdValue);
+    }
+
+    /////////////////////////////////////////////////
+    // Deposit Collateral tests                    //
+    /////////////////////////////////////////////////
+
+    function testRevertsIfCollateralIsZero() public {
+        vm.startPrank(USER);
+        ERC20Mock(weth).approve(address(rscEngine), AMOUNT_COLLATERAL);
+
+        vm.expectRevert(RSCEngine.RSCEngine__NeedsmoreThanZero.selector);
+        rscEngine.depositCollateral(weth, 0);
+        vm.stopPrank();
     }
 }
